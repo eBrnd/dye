fc37536c7ed53aa3c93e27e22176f9a6ae8b22bd
eBrnd <github@ebrnd.de>
2015-04-03 03:14:48 +0200

    add optional red color

--- colorpipe.c
+++ colorpipe.c
@@ -1,13 +1,20 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 char color_normal[] = "\033[0m";
 char color_green[] = "\033[32m";
+char color_red[] = "\033[31m";
 
 int main(int argc, char** argv) {
   char color_string[sizeof(color_green) + 1 + sizeof(color_normal)];
   size_t output_pos = sizeof(color_green);
-  strcpy(color_string, color_green);
+
+  if (argc > 1 && !strcmp(argv[1], "-red"))
+    strcpy(color_string, color_red);
+  else
+    strcpy(color_string, color_green);
+
   strcpy(color_string + output_pos + 1, color_normal);
 
   char c;
