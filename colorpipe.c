f8babd0c0225ce046204a2a95cf36a77401d880f
eBrnd <github@ebrnd.de>
2015-04-03 03:43:35 +0200

    more color command line arguments

--- colorpipe.c
+++ colorpipe.c
@@ -2,23 +2,35 @@
 #include <string.h>
 #include <stdio.h>
 
-char color_normal[] = "\033[0m";
-char color_green[] = "\033[32m";
-char color_red[] = "\033[31m";
+const char* choose_color(const char* arg) {
+  if (!strcmp(arg, "red")) return "\033[31m";
+  if (!strcmp(arg, "green")) return "\033[32m";
+  if (!strcmp(arg, "brown")) return "\033[33m";
+  if (!strcmp(arg, "blue")) return "\033[34m";
+  if (!strcmp(arg, "magenta")) return "\033[35m";
+  if (!strcmp(arg, "cyan")) return "\033[36m";
+  if (!strcmp(arg, "white")) return "\033[37m";
+  return "";
+}
+
+const char color_normal[] = "\033[39m";
+const size_t colstr_len = sizeof(color_normal); // Assumes that all the color strings above are the same size as color_normal.
 
 int main(int argc, char** argv) {
-  char color_string[sizeof(color_green) + 1 + sizeof(color_normal)];
-  size_t output_pos = sizeof(color_green);
+  char color_string[2*colstr_len + 1];
+  size_t output_pos = colstr_len;
 
-  if (argc > 1 && !strcmp(argv[1], "-red"))
-    strcpy(color_string, color_red);
+  if (argc > 1)
+    strcpy(color_string, choose_color(argv[1]));
   else
-    strcpy(color_string, color_green);
+    strcpy(color_string, choose_color("green"));
+
 
   strcpy(color_string + output_pos + 1, color_normal);
 
   char c;
   ssize_t n;
+
   while (0 < (n = read(0, &c, 1))) {
     color_string[output_pos] = c;
     write(1, color_string, sizeof(color_string));
